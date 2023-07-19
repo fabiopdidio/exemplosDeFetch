6790901eaa26e83a24df6d683dede4f81847ac6c
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-07-19 14:58:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// Vai no HTML, pega o elemento cepForm e adiciona um evento de escuta para submit para exibir dados
+// Vai no HTML, pega o elemento cepForm e adiciona um evento de escuta para submit para exibir dados.
 document.getElementById("cepForm").addEventListener("submit", exibirDados);
 
 function exibirDados(event) {
-  event.preventDefault();
+  event.preventDefault(); // Impede o recarregamento da tela.
 
-  const cep = document.getElementById("cep").value;
+  const cep = document.getElementById("cep").value; // Variável para pegar o valor digitado no input.
 
   // URL -> viabrasil.com.br/ws/01001000/json/
 
-  if (cep.length === 8 || cep.length === 9) {
-    fetch(`https://viacep.com.br/ws/${cep}/json/`) // GET
+  if (cep.length === 8 || cep.length === 9) { // Delimita um númeo de caracteres para 8 ou 9 (aceitando "-")
+    fetch(`https://viacep.com.br/ws/${cep}/json/`) // GET (precisa utilizar https://)
       .then((response) => response.json())
       .then((data) => {
         const resultDiv = document.getElementById("result");
